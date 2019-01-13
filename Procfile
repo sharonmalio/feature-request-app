e1d36541b9296e54641a586d37479cce9b2fd179
smalio <kaninimalio@gmail.com>
2019-01-13 20:22:39 +0300

    deploying on heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
- web: python featurequest.py
+web: gunicorn featurequest:app
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,12 @@
-from features import app
 
+import sys
+
+# add your project directory to the sys.path
+project_home = u'/home/dmueni/feature-request-app'
+if project_home not in sys.path:
+     sys.path = [project_home] + sys.path
+
+# import flask app but need to call it "application" for WSGI to work
+from features import app as application
 if __name__ == "__main__":
     app.run()
