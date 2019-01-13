5f2d16ca34842a43fcdd500e2186ca6ac585e6f3
smalio <kaninimalio@gmail.com>
2019-01-13 20:14:33 +0300

    deploying on heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn featurequest:app
+ web: python featurequest.py
--- featurequest.py
+++ featurequest.py
@@ -6,4 +6,6 @@ def make_shell_context():
     return {'db': db, 'User': User, 'Feature': Feature, 'Client':Client}
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+     app.debug = True
+     port = int(os.environ.get("PORT", 5000))
+     app.run(host='0.0.0.0', port=port)
